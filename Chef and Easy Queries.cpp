2878ea703278dbeb17c1a76a6f5fd1ece5f6c764
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-03 21:34:31 +0530

    commited

--- Chef and Easy Queries.cpp
+++ Chef and Easy Queries.cpp
@@ -9,28 +9,28 @@ int main()
     {
         long long int n,k;
         cin>>n>>k;
-        long long int pen=0;
+        long long int pending_queries=0;
         bool f=false;
         long long int day=0;
         for(long long int i=0;i<n;i++)
         {
             long long int x;
             cin>>x;
-            pen=pen+x;
-            if(pen<k and !f)
+            pending_queries=pending_queries+x;
+            if(pending_queries<k and !f)
             {
                 day=i+1;
                 f=true;
             }
-            pen=pen-k;
-            if(pen<0)
+            pending_queries=pending_queries-k;
+            if(pending_queries<0)
             {
-                pen=0;
+                pending_queries=0;
             }
         }
         if(!f)
         {
-            day=n+1+pen/k;
+            day=n+1+pending_queries/k;
             cout<<day<<endl;
         }
         else
