bf1c4720cd1e6d0033b9f7ff77de555061772592
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-01-01 21:33:36 +0530

    commited

---
+++ Codechef/One Zero Swaps.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,cs=0,cp=0;
+        cin>>n;
+        string s,p;
+        cin>>s;
+        cin>>p;
+        
+        for(int i=0;i<n;i++)
+        {
+            
+        }
+
+
+
+
+        if(cs==cp)
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+        }
+
+
+       // cout<<s<<" and "<<p<<endl;
+
+      //  cout<<cs<<" and "<<cp<<endl;
+
+    }    
+}
+
