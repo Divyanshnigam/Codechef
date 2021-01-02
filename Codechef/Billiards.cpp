4419679fc58775c5eedf08a90c36fc6e3b70c899
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-01-02 21:14:53 +0530

    Billiards

---
+++ Codechef/Billiards.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,x,y;
+        cin>>n>>k>>x>>y;
+        while(k)
+        {
+            
+            if((x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+            {
+                break;
+            }
+            else if(x==n and y!=0)
+            {
+                x--;
+                y++;
+                k--;
+            }
+            else if(x!=0 and y==n)
+            {
+                x--;
+                y--;
+                k--;
+            }
+            else if(x==0 and y!=0)
+            {
+                x++;
+                y--;
+                k--;
+            }
+            else if(x!=0 and y==0)
+            {
+                x++;
+                y++;
+                k--;
+            }
+            x++;
+            y++;
+        }
+        cout<<x<<" "<<y<<endl;
+    }    
+}
