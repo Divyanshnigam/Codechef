7552d294d7388bec41a72f9fee99fef4d0866602
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-01-03 01:08:16 +0530

    billards

--- Codechef/Billiards.cpp
+++ Codechef/Billiards.cpp
@@ -8,40 +8,148 @@ int main()
     {
         int n,k,x,y;
         cin>>n>>k>>x>>y;
-        while(k)
+        if(k > 4)
         {
-            
-            if((x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
-            {
-                break;
-            }
-            else if(x==n and y!=0)
-            {
-                x--;
-                y++;
-                k--;
-            }
-            else if(x!=0 and y==n)
-            {
-                x--;
-                y--;
-                k--;
-            }
-            else if(x==0 and y!=0)
-            {
-                x++;
-                y--;
-                k--;
-            }
-            else if(x!=0 and y==0)
-            {
-                x++;
-                y++;
-                k--;
-            }          //need to iterate ass possibily
-            x++;
-            y++;
+           k = k%4;
         }
-        cout<<x<<" "<<y<<endl;
-    }    
-}
+        
+        if(x == y)                            //case 1:
+        {
+            cout<<n<<" "<<n<<endl;
+        }
+        else                                  // case 2:
+        {
+            if(x<y)
+                {
+                //  cout<<x<<" "<<y<<endl;
+                while(k)
+                {
+                    // cout<<x<<" "<<y<<endl;
+                    
+                    while(y != n)
+                    {
+                    
+                        x++;
+                        y++;
+                        // cout<<x<<" "<<y<<endl;
+                    }
+                    
+                    k--;
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        //   cout<<x<<" break "<<y<<endl;
+                        break;
+                    }
+                    
+                    while(x != n)
+                    {
+                        
+                        x++;
+                        y--;
+                        // cout<<x<<" "<<y<<endl;
+                        
+                    }
+                    
+                    k--;
+                    
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                    while(y != 0)
+                    {
+                        x--;
+                        y--;
+                    //     cout<<x<<" "<<y<<endl;
+                        
+                        
+                    }
+                    
+                    k--;  
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                    while(x != 0)
+                    {
+                        x--;
+                        y++;
+                        
+                        // cout<<x<<" "<<y<<endl;
+                        
+                    }
+                    k--;
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                
+                }
+                cout<<x<<" "<<y<<endl;
+            }
+            else
+            {
+                while(k)
+                {
+                    // cout<<x<<" "<<y<<endl
+                    
+                    while(x != n)
+                    {
+                        
+                        x++;
+                        y++;
+                    //    cout<<x<<" "<<y<<endl;
+                        
+                    }
+                    k--;
+                    
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                    
+                    while(y != n)
+                    {
+                    
+                        x--;
+                        y++;
+                        // cout<<x<<" "<<y<<endl;
+                    }
+                    k--;
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        //   cout<<x<<" break "<<y<<endl;
+                        break;
+                    }
+                    while(x != 0)
+                    {
+                        x--;
+                        y--;
+                    //     cout<<x<<" "<<y<<endl;
+                        
+                        
+                    }
+                    k--;  
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                    
+                    while(y != 0)
+                    {
+                        x++;
+                        y--;
+                        // cout<<x<<" "<<y<<endl;
+                        
+                    }
+                    k--;
+                    if(k==0 or (x==n and y==n) or (x==0 and y==n) or (x==n and y==0) or (x==0 and y==0))
+                    {
+                        break;
+                    }
+                }
+            cout<<x<<" "<<y<<endl;
+            }
+        }
+    }
+}    
