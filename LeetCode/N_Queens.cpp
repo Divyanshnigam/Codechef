006fe0ba624267573e35c6974508c5517ec67d8e
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-01-06 23:25:59 +0530

    N-Queens

---
+++ LeetCode/N_Queens.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void check(int *cb,int r,string asf)
+    {
+        if(n==r)
+        {
+            return;
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(safe(cb[i][j]=false)
+            {
+                check(cb,r+1,)
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) 
+    {
+        char cb[n][n]={0};
+        check(cb,n,0,"");
+        return check;
+        
+    }
+};
